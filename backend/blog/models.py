fa031512a393e3fd53b1a00f64fb59eb2ef7570d
Danila Volkov <noxlovette@proton.me>
2024-12-14 17:13:41 +0000

    start work

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,54 +1,23 @@
 from django.db import models
 from django.utils.text import slugify
 import uuid
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
-class Article(models.Model):
+class Task(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
     title = models.CharField(max_length=255)
-    content = models.TextField(help_text="Markdown and LaTeX raw content", blank=True)
-    processed_html = models.TextField(help_text="HTML content processed from markdown and LaTeX", blank=True)
+    description = models.TextField()
+    priority = models.PositiveSmallIntegerField(
+        default=1,
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(max_length=50, default='pending')
 
-    category= models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
-    tags = models.ManyToManyField('Tag', blank=True, null=True, related_name='articles')
-    concepts = models.ManyToManyField('Concept', blank=True, related_name='articles')
-
-    related_articles = models.ManyToManyField('self', blank=True, symmetrical=True)
-
-    slug = models.SlugField(max_length=255, unique=True, help_text="URL friendly title", blank=True)
-
-    difficulty = models.IntegerField(default=0, help_text="Difficulty level from 0 to 10")
-
-    published = models.BooleanField(default=False)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
+    assignee = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='tasks')
 
     def __str__(self):
         return self.title
-    
-class Category(models.Model):
-    # A large concept, such as organic chemistry
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    name = models.CharField(max_length=255)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
-    
-class Tag(models.Model):
-    # a very small concept, such as benzol
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-class Concept(models.Model):
-    # Electronegativity, atomic structure
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
