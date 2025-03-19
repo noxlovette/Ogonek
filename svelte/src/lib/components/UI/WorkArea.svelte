9b300dcea87b0fb6155ee3be62f8aa8660572a4e
Danila Volkov <noxlovette@proton.me>
2025-03-19 11:15:01 +0100

    versions, small workarea fix

--- svelte/src/lib/components/UI/WorkArea.svelte
+++ svelte/src/lib/components/UI/WorkArea.svelte
@@ -3,10 +3,10 @@
 </script>
 
 <div
-  class="md: relative flex
-h-max
-  min-h-full flex-1 scroll-my-6 flex-col gap-2 rounded-lg bg-white
-  px-5 py-3 pb-24 shadow-sm ring ring-stone-200 md:gap-4
+  class="relative flex h-max
+min-h-full
+  flex-1 scroll-my-6 flex-col gap-2 rounded-lg bg-white px-5
+  py-3 pb-24 shadow-sm ring ring-stone-200 md:-translate-y-3 md:gap-4
   dark:bg-stone-950 dark:ring-stone-900
   "
 >
