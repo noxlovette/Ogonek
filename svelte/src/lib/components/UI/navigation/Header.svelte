6f242de85b2c9ae71a6552dd75ff2240bb19c1ec
Danila Volkov <noxlovette@proton.me>
2025-03-19 11:22:07 +0100

    header fix

--- svelte/src/lib/components/UI/navigation/Header.svelte
+++ svelte/src/lib/components/UI/navigation/Header.svelte
@@ -9,7 +9,9 @@
 
 <header class="w-full items-baseline">
   <div class="mx-auto flex w-full items-center justify-between px-4 py-3">
-    <div class="z-50 flex w-full items-center justify-center md:w-1/6">
+    <div
+      class="z-50 flex w-full items-center justify-center md:w-1/6 md:items-start md:justify-start"
+    >
       <a {href} class="font-serif text-2xl font-bold">Ogonek</a>
     </div>
 
