0b1878c22104b6f740b50757c680c8ae386165fd
Danila Volkov <noxlovette@Thor.local>
2025-04-28 15:18:26 +0300

    cloze sentences init

---
+++ svelte/src/lib/utils/constants.ts
@@ -0,0 +1,38 @@
+export const qualityButtons = [
+  {
+    quality: 0,
+    label: "1066",
+    color: "ring-red-600 hover:bg-red-700/10 ring",
+    key: 1,
+  },
+  {
+    quality: 1,
+    label: "Wrong",
+    color: "ring-orange-500 hover:bg-orange-600/10 ring",
+    key: 2,
+  },
+  {
+    quality: 2,
+    label: "Hard",
+    color: "ring-yellow-500 hover:bg-yellow-600/10 ring",
+    key: 3,
+  },
+  {
+    quality: 3,
+    label: "Okay",
+    color: "ring-stone-400 hover:bg-stone-400/10 ring",
+    key: 4,
+  },
+  {
+    quality: 4,
+    label: "Good",
+    color: "ring-green-500 hover:bg-green-600/10 ring",
+    key: 5,
+  },
+  {
+    quality: 5,
+    label: "Easy",
+    color: "ring-cacao-500 hover:bg-cacao-600/10 ring",
+    key: 6,
+  },
+];
