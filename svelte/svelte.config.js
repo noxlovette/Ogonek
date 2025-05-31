d8af26da3492404d80d89006e0b028b7e1da22c7
Danila Volkov <noxlovette@proton.me>
2025-05-31 17:13:18 +0300

    removed csp

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -9,17 +9,6 @@ const config = {
       out: "build",
       precompress: false,
     }),
-    csp: {
-      directives: {
-        "script-src": ["self", "https://browser.sentry-cdn.com"],
-        "connect-src": ["self", "https://*.ingest.sentry.io"],
-      },
-      reportOnly: {
-        "script-src": ["self", "https://browser.sentry-cdn.com"],
-        "connect-src": ["self", "https://*.ingest.sentry.io"],
-        "report-uri": [process.env.SENTRY_REPORT_URL],
-      },
-    },
   },
 };
 
