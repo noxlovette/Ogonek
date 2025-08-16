2ee3253a6e83777bd6f01c5f24e37ba7fcacefce
Danila Volkov <noxlovette@proton.me>
2025-08-16 20:35:27 +0300

    minor openapi shit

--- axum/src/main.rs
+++ axum/src/main.rs
@@ -35,7 +35,7 @@ async fn run_server() -> anyhow::Result<()> {
         .nest("/decks", deck_routes())
         .nest("/s3", s3_routes())
         .nest("/learn", learn_routes())
-        .nest("/notification", notification_routes())
+        .nest("/notifications", notification_routes())
         .nest("/state", state_routes());
 
     // Public routes (no auth required)
