5f8da4753f74c5918326f3229c7d85995e0bf59e
Danila Volkov <noxlovette@proton.me>
2025-08-31 18:16:59 +0300

    notification display fix

--- svelte/src/lib/components/UI/NotificationBadge.svelte
+++ svelte/src/lib/components/UI/NotificationBadge.svelte
@@ -6,11 +6,11 @@
 
 {#if badgeVisible}
   <div
-    class="h-4flex absolute -top-1 -right-1 min-w-[24px] animate-pulse items-center justify-center rounded-full border border-red-400/20
-            bg-red-500 px-2
-            text-xs font-bold text-white hover:animate-none
-            md:ml-2 md:block
-            md:h-6 dark:bg-red-600
+    class="absolute -top-1 -right-1 h-4 animate-pulse items-center justify-center rounded-full border border-red-400/20 bg-red-500 px-2 text-xs font-bold
+            text-white hover:animate-none
+            md:relative md:-top-0 md:-right-0 md:ml-2
+            md:flex md:h-6
+            md:min-w-[24px] dark:bg-red-600
           "
   >
     {badge > 99 ? "99+" : badge}
